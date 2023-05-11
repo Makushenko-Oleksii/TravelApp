8340fce5d608e103afafa450336b7139f2c22357
Makushenko Oleksii <makushenkooleksii@gmail.com>
2023-05-11 16:09:36 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+import Home from "./src/screens/Home";
+
+
+function App() {
+  return (
+      <Home/>
+  );
+}
+
+export default App;
---
+++ src/components/Title/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Text} from 'react-native';
+import styles  from "./styles";
+const Title = () => {
+  return (
+    <Text  style={styles.title}>
+        This title
+    </Text>
+  );
+};
+
+export default Title;
